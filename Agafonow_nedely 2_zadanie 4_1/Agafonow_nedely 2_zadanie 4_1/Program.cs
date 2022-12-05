4a541926cb5e1d6318d429cf3e85ad7a6b383a7e
dima20004 <117287272+dima20004@users.noreply.github.com>
2022-12-05 22:26:49 +0300

    Add files via upload

---
+++ Agafonow_nedely 2_zadanie 4_1/Agafonow_nedely 2_zadanie 4_1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agafonow_nedely_2_zadanie_4_1
+{
+    internal class Program
+    {
+        private static int readInt()
+        {
+            do
+            {   //Проверка на ввод данных
+                int result;
+                bool parsed = int.TryParse(Console.ReadLine(), out result);
+                if (parsed) return result;
+                else Console.Write("Повторите попытку (введите целое число) n=");
+
+            } while (true);
+        }
+
+            static int recurs(int n)
+        {
+            int b1 = -10;
+            int b2 = 2;
+            if (n <= 0)
+                return 0;
+            if (n == 1)
+                return b1;
+            if (n == 2)
+                return b2;
+            return Math.Abs(recurs(n - 2)) - 6 * recurs(n - 1);
+    }
+
+    static void Main(string[] args)
+        {
+            bool a = true;
+            int n=0;
+            while (a)
+            {
+                Console.Write("Введите n=");
+                n = readInt();
+                if (n < 1) Console.WriteLine("n>0!");
+                else a = false;
+            }
+            Console.WriteLine("Значение={0}", recurs(n));
+
+        }
+
+    }
+}
---
+++ Agafonow_nedely 2_zadanie 4_2/Agafonow_nedely 2_zadanie 4_2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agafonow_nedely_2_zadanie_4_2
+{
+
+
+    internal class Program
+    {
+       
+
+        private static double readDoule()
+        {
+            do
+            {   //Проверка на ввод данных
+                double result;
+                bool parsed = double.TryParse(Console.ReadLine(), out result);
+                if (parsed) return result;
+                else Console.Write("Введите число заново=");
+
+            } while (true);
+        }
+
+
+        static void Rec(double n, ref double x, double d, ref double s)
+        {
+            if (n > 0)
+            {
+                s += x;
+                x += d;
+
+                Rec(n - 1, ref x, d, ref s);
+            }
+        }
+
+       
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первый член арифметической прогрессии:");
+            double n1 = readDoule();
+            Console.Write("Введите разность арифметической прогрессии:");
+            double d = readDoule();
+            bool r = true;
+
+            double n=0;
+            while (r)
+            {
+                Console.Write("Введите n-й член прогрессии:");
+                n = readDoule();
+                if (n < 1) Console.WriteLine("n>1!");
+                if (n >= 1) r = false;
+            }
+       
+
+           
+            double s = 0;
+            Rec(n, ref n1, d, ref s);
+            Console.WriteLine($"{n}-й член прогрессии={n1-d}, сумма {n} первых членов прогрессии s={s}");
+
+        }
+    }
+}
