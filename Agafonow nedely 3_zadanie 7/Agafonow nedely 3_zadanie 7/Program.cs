b10c926277dd702580630cc6bf89d428648aefd3
dima20004 <117287272+dima20004@users.noreply.github.com>
2022-12-12 15:02:08 +0300

    Add files via upload

---
+++ Agafonow nedely 3_zadanie 7/Agafonow nedely 3_zadanie 7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agafonow_nedely_3_zadanie_7
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            StringBuilder a = new StringBuilder(Console.ReadLine());
+            Console.WriteLine("Исходная строка: " + a);
+
+           
+            for (int i = 0; i < a.Length-1; i=i+2)
+            {
+                if (a[i] == ' ' || a[i + 1] == ' ')
+                {
+                    i=i - 1;
+                }
+                else
+                {
+                    char buf = a[i];
+                    buf = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = buf;
+                }
+               
+            }
+               
+            Console.WriteLine("Измененная строка: " + a);
+        }
+
+    }
+}
+
---
+++ Agafonow_nedely 3_zadanie 7_2/Agafonow_nedely 3_zadanie 7_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agafonow_nedely_3_zadanie_7_2
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            string a=Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Исходная строка: " + Convert.ToString(a));
+
+            Console.WriteLine("Введите количество символов n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string[] myStrings;
+            myStrings = a.Split(' ');
+            for (int i = 0; i < myStrings.Length; i++)
+            {
+                if (myStrings[i].Length > n)
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine(myStrings[i]);
+                }
+
+            }
+
+
+        }
+    }
+}
